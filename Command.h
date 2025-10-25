5d3ac747c186f1ad2953e4e1c4c3d3ff94a76611
LuyandaNdlovu-4G <u24580717@tuks.co.za>
2025-10-25 11:28:46 +0200

    Updated Command.h fie

--- Command.h
+++ Command.h
@@ -1,18 +1,34 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
+#include "PlantProduct.h"
+
+#include <string>
+#include <map>
+#include <iostream>
+
 /**
  * @brief Abstract Command class (Command pattern)
  * Defines the interface for executing and undoing commands
  */
 
 class Command {
-public:
-    virtual ~Command() = default;
 
-    // Command interface
-    virtual void execute() = 0;
-    virtual void undo();
+    private:
+        //static map of command prototypes, keyed by a string type
+        static std::map<std::string, Command*> prototypes;
+
+    public:
+        virtual ~Command() = default;
+
+        // Command interface
+        virtual void execute() = 0;
+        virtual std::string getType() const = 0;
+        virtual std::string getRequiredRole() const = 0;
+        virtual Command* clone() const = 0;
+        static void registerCommand(const std::string& type, Command* prototype);
+        static Command* createCommand(const std::string& type);
+        virtual void undo();
 };
 
 #endif
